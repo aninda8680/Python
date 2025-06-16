a7b2b272256166876206f09ca0fb6944c479af69
aninda8680 <anindadebta8680@gmail.com>
2025-06-16 21:53:30 +0530

    First Commit

---
+++ Day1.1.py
@@ -0,0 +1,14 @@
+#type() => checks the type of output
+name = "Aninda"
+print(type(name))
+
+
+
+# Input/Output
+
+name = input('Enter the name:')
+age = int(input("Enter the age:"))
+
+print("Hello", name)
+print("Your age is ", age)
+print(f"Your age is {age}")
