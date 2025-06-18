3b718656283a9107c5b8a1e92ca0c2e8be6d010b
aninda8680 <anindadebta8680@gmail.com>
2025-06-18 21:31:59 +0530

    TUPLE

---
+++ Day2.3.py
@@ -0,0 +1,9 @@
+#TUPLE
+# A Tuple is immutable , ordered, indexed collection of item, like lists
+# It can store heterogenous data.
+
+my_tuple = (1, 2,  2, "hello", 3.14)
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple.index("hello"))
+print(my_tuple.count(2))
